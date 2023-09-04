837025e2cbf6f5b5c28a191a0573b5a643d2a2f4
Ahmad Rizki <amrizjr@gmail.com>
2023-09-04 22:06:46 +0700

    tes

--- src/pages/wifi.tsx
+++ src/pages/wifi.tsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 const Wifi = () => {
@@ -8,7 +8,7 @@ const Wifi = () => {
   const handleSSIDChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedSSID(event.target.value);
   };
-  
+
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
@@ -24,30 +24,32 @@ const Wifi = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="font-bold text-center my-16 text-4xl">SSID</h1>
-        <div>
-          <label htmlFor="ssidDropdown">SSID:</label>
+      <h1 className="font-bold text-center my-16 text-4xl">SSID</h1>
+      <div className="w-full md:w-5/6 lg:w-2/3 mx-auto xl:w-3/6 min-h-screen">
+        <div className="w-full px-4 mb-8">
+          <label htmlFor="ssidDropdown" className="text-base text-sky-400 font-bold">SSID</label>
           <select
             id="ssidDropdown"
             onChange={handleSSIDChange}
             value={selectedSSID}
+            className="w-full bg-neutral-200 text-dark p-3 rounded-md focus:outline-none focus:ring-sky-400 focus:ring-1 focus:border-sky-400"
           >
             <option value="SiPalingPaling">SiPalingPaling</option>
             <option value="Halleluya">Halleluya</option>
           </select>
         </div>
-        <div>
-          <label htmlFor="passwordInput">Password:</label>
+        <div className="w-full px-4 mb-8">
+          <label htmlFor="passwordInput" className="text-base text-sky-400 font-bold">Password</label>
           <input
             type="text"
             id="passwordInput"
             value={password}
             onChange={handlePasswordChange}
             placeholder="Enter password"
+            className="w-full bg-neutral-200 text-dark p-3 rounded-md focus:outline-none focus:ring-sky-400 focus:ring-1 focus:border-sky-400 invalid:focus:ring-pink-700 invalid:focus:border-pink-700"
           />
         </div>
-        <button className="bg-sky-500 px-3" onClick={handleRegister}>
+        <button className="text-base font-semibold bg-sky-400 text-white py-2 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500" onClick={handleRegister}>
           Register
         </button>
       </div>
--- src/pages/wifiRegister.tsx
+++ src/pages/wifiRegister.tsx
@@ -1,12 +1,59 @@
+import { useState, ChangeEvent, FormEvent } from "react";
 import Navbar from "../components/Navbar";
 
-const WifiRegister = () => {
-  return (
-    <div>
-      <Navbar />
-      <h1>Halaman Wifi Register</h1>
-    </div>
-  );
-}
+const WifiRegistration: React.FC = () => {
+  const [ssidList, setSsidList] = useState<string[]>([]);
+  const [newSsid, setNewSsid] = useState<string>("");
 
-export default WifiRegister;
+  const handleSsidChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewSsid(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (newSsid.trim() !== "") {
+      setSsidList([...ssidList, newSsid]);
+      setNewSsid("");
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <h1 className="font-bold text-center my-16 text-4xl">WiFi Registration</h1>
+      <div className="w-full md:w-5/6 lg:w-2/3 mx-auto xl:w-3/6 min-h-screen">
+        <form onSubmit={handleSubmit}>
+          <div className="w-full px-4 mb-8">
+            <label htmlFor="ssidInput" className="text-base text-sky-400 font-bold">
+              Enter SSID
+            </label>
+            <input
+              type="text"
+              id="ssidInput"
+              value={newSsid}
+              onChange={handleSsidChange}
+              placeholder="Enter SSID"
+              className="w-full bg-neutral-200 text-dark p-3 rounded-md focus:outline-none focus:ring-sky-400 focus:ring-1 focus:border-sky-400"
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-base font-semibold bg-sky-400 text-white py-2 px-8 rounded-full w-full hover:opacity-80 hover:shadow-lg transition duration-500"
+          >
+            Add SSID
+          </button>
+        </form>
+        <div className="mt-8">
+          <h2 className="text-base text-sky-400 font-bold">SSID List:</h2>
+          <ul className="list-disc ml-4">
+            {ssidList.map((ssid, index) => (
+              <li key={index}>{ssid}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WifiRegistration;
